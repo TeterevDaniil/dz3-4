cc2fd564e8041d42c48a2dd99210df75813c34f5
Даниил Александрович Тетерев <104613635+TeterevDaniil@users.noreply.github.com>
2022-05-09 15:45:41 +0600

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,9 @@ const db = require('../db')
 
 
 router.get('/', (req, res, next) => {
+  if (req.session.isAdmin) {
+    res.redirect('/admin');
+  }
   res.render('pages/login', { title: 'SigIn page' })
 })
 
@@ -11,10 +14,18 @@ router.post('/', (req, res, next) => {
   // TODO: Реализовать функцию входа в админ панель по email и паролю
   const email = req.body.email;
   const password = req.body.password;
-  console.log(email);
-  console.log(password);
-   console.log(db.getState().users);
-  res.send('Реализовать функцию входа по email и паролю')
+  const userPass = db.get('users[0]._password').value()
+  const userEmail = db.get('users[0]._email').value()
+
+  if (userEmail === email && userPass === password) {
+    req.session.isAdmin = true;
+    res.redirect('/admin');
+  }
+  else {
+    req.flash('msglogin', 'Неправильный логин или пароль');
+    res.redirect('/login');
+  }
+
 })
 
 module.exports = router
--- routes/main.js
+++ routes/main.js
@@ -15,26 +15,21 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
   // TODO: Реализовать функционал отправки письма.
   if (!req.body.name || !req.body.email || !req.body.message) {
-    return res.json({ msgemail: 'Необходимо заполнить все поля', status: 'Error' })
-   
+    res.flash('msgemail', 'Необходимо заполнить все поля!');
   }
   const transporter = nodemailer.createTransport(config.mail.smtp)
   const mailOptions = {
     from: `"${req.body.name}" <${req.body.email}>`,
     to: config.mail.smtp.auth.user,
     subject: config.mail.subject,
-    text:  req.body.message.trim().slice(0, 500) +
+    text: req.body.message.trim().slice(0, 500) +
       `\n Отправлено с: <${req.body.email}>`
   }
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      return res.json({
-        msg: `При отправке письма произошла ошибка!: ${error}`,
-        status: 'Error'
-      })
+      res.flash('msgemail', 'При отправке письма произошла ошибка!');
     }
-    res.json({ msgemail: 'Письмо отправленно', status: 'OK' })
-    //res.flash('msgemail', 'Письмо успешно отправлено!');
+    res.flash('msgemail', 'Письмо успешно отправлено!');
   })
 })
 
